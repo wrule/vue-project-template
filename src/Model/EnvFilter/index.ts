2631123d22b2594da69ffd89b2dceda6b96fb5d5
鸡毛 <shihao.gu@perfma.com>
2020-01-03 14:58:40 +0800

    完善环境过滤器

--- src/Model/EnvFilter/index.ts
+++ src/Model/EnvFilter/index.ts
@@ -20,11 +20,17 @@ export default class EnvFilter {
   public get CoreNum(): number {
     return Math.floor(this.coreNum);
   }
+  public set CoreNum(nv: number) {
+    this.coreNum = Math.floor(nv);
+  }
   /**
    * 内存大小
    */
   public get MemSize(): number {
     return Math.floor(this.memSize);
+  }
+  public set MemSize(nv: number) {
+    this.memSize = Math.floor(nv);
   }
   /**
    * 内存大小单位
@@ -32,11 +38,17 @@ export default class EnvFilter {
   public get MemUnit(): EnvUnit {
     return this.memUnit;
   }
+  public set MemUnit(nv: EnvUnit) {
+    this.memUnit = nv;
+  }
   /**
    * 操作系统类型
    */
   public get OS(): EnvOS {
     return this.os;
+  }
+  public set OS(nv: EnvOS) {
+    this.os = nv;
   }
   /**
    * 操作系统字长
@@ -44,11 +56,17 @@ export default class EnvFilter {
   public get WordSize(): EnvWordSize {
     return this.wordSize;
   }
+  public set WordSize(nv: EnvWordSize) {
+    this.wordSize = nv;
+  }
   /**
    * JDK版本
    */
   public get JDKVersion(): EnvJDKVersion {
     return this.jdkVersion;
+  }
+  public set JDKVersion(nv: EnvJDKVersion) {
+    this.jdkVersion = nv;
   }
   /**
    * 内存大小字符串
