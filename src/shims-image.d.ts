3f3db4fa018df98861b5bbbe8bd0d14d777e1360
鸡毛 <shihao.gu@perfma.com>
2020-01-09 10:02:15 +0800

    图片和媒体文件声明

---
+++ src/shims-image.d.ts
@@ -0,0 +1,26 @@
+/**
+ * This file is part of the PerfMa.
+ * @link     : http://perfma.com
+ * @author   : William Chan (root@williamchan.me)
+ * @copyright: Copyright (c) 2019 Hangzhou perfma Network Technology Co., Ltd.
+ */
+declare module "*.jpg" {
+  const value: any;
+  export = value;
+}
+declare module "*.jpeg" {
+  const value: any;
+  export = value;
+}
+declare module "*.png" {
+  const value: any;
+  export = value;
+}
+declare module "*.gif" {
+  const value: any;
+  export = value;
+}
+declare module "*.svg" {
+  const value: any;
+  export = value;
+}
---
+++ src/shims-media.d.ts
@@ -0,0 +1,34 @@
+/**
+ * This file is part of the PerfMa.
+ * @link     : http://perfma.com
+ * @author   : William Chan (root@williamchan.me)
+ * @copyright: Copyright (c) 2019 Hangzhou perfma Network Technology Co., Ltd.
+ */
+declare module "*.mp4" {
+  const value: any;
+  export = value;
+}
+declare module "*.webm" {
+  const value: any;
+  export = value;
+}
+declare module "*.ogg" {
+  const value: any;
+  export = value;
+}
+declare module "*.mp3" {
+  const value: any;
+  export = value;
+}
+declare module "*.wav" {
+  const value: any;
+  export = value;
+}
+declare module "*.flac" {
+  const value: any;
+  export = value;
+}
+declare module "*.aac" {
+  const value: any;
+  export = value;
+}
