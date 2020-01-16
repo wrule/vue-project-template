2a352b9949f7af5dfef65fec094f33731728001f
鸡毛 <shihao.gu@perfma.com>
2020-01-16 17:52:17 +0800

    尝试编写searcher组件

--- src/components/searcher/index.tsx
+++ src/components/searcher/index.tsx
@@ -1,16 +1,17 @@
 import { Component, Vue } from 'vue-property-decorator';
 import { VNode } from 'vue';
 import style from './index.mod.scss';
-import VSearcher from '@/components/searcher';
 
 @Component
-export default class ViewIndex extends Vue {
+export default class VSearcher extends Vue {
+  private type: string = '';
+  private keyword: string = '';
   // eslint-disable-next-line class-methods-use-this
   public render(): VNode {
     return (
-      <div class={style.view}>
-        <img src="panda.jpg" />
-        <VSearcher />
+      <div class={style.com}>
+        <input type="number" v-model={this.keyword} />
+        <button>搜索</button>
       </div>
     );
   }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import Vue from 'vue';
 import App from './app/index';
 import router from '@/router';
 import ':/iconfont/iconfont.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI, {
+  size: 'mini',
+});
 
 new Vue({
   el: '#vue-app',
